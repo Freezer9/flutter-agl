3806d9e52de594e1d74fb5a462f66eef35ee7227
Freeze <freeze@agl.local>
2025-11-30 12:16:00 +0700

    fix: remove device preview

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,3 @@
-import 'package:device_preview/device_preview.dart';
-
 import 'export.dart';
 import 'data/data_providers/initialize_settings.dart';
 
@@ -14,14 +12,7 @@ void main() async {
   runApp(
     ProviderScope(
       parent: container,
-      child: DevicePreview(
-        enabled: debugDisplay,
-        tools: const [
-          ...DevicePreview.defaultTools,
-        ],
-        builder: (context) => const App(),
-      ),
+      child: const App(),
     ),
   );
 }
-
